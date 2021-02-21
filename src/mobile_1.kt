8cdb04e678b60fc86e668ead55f31b291882544c
fadallalaaisha <aishafadallala@gmail.com>
2021-02-21 10:50:20 +0300

    my first assignment

---
+++ src/mobile_1.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val name="Aisha Hamad"
+    var age:Int=27
+    var phoneNumber="0706707656"
+    var weight=77
+    val kenyanCitizen:Boolean=false
+    println(age)
+    println(weight)
+    println(name)
+    println(phoneNumber)
+    println(kenyanCitizen)
+}
